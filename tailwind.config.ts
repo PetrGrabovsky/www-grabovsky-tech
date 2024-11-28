002039a6297d280216a6c8a738795db98fa2fd25
Petr Grabovský <petr@grabovsky.tech>
2024-11-28 12:58:18 +0100

    style: Přidej definice pro screens a container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './layout/**/*.{ts,tsx}', './shared/**/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -9,6 +9,20 @@ export default {
       },
       backgroundImage: {
         'white-carbon': 'url("/white-carbon.png")',
+      },
+      screens: {
+        '2xl': '1280px',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1.5rem',
+          md: '2rem',
+          lg: '2.5rem',
+          xl: '3rem',
+          '2xl': '3.5rem',
+        },
       },
     },
   },
