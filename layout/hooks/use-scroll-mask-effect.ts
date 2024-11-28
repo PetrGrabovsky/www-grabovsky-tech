3fa17d7d7214f21c03c3bccc5cde213533ebe69e
Petr Grabovský <petr@grabovsky.tech>
2024-11-28 21:19:06 +0100

    feat: Vytvoř hook pro manipulaci s maskou komponenty Main

--- layout/hooks/use-scroll-mask-effect.ts
+++ layout/hooks/use-scroll-mask-effect.ts
@@ -1,18 +1,18 @@
 import { RefObject, useCallback, useEffect, useRef } from 'react';
 
 const useScrollMaskEffect = (mainReference: RefObject<HTMLElement>) => {
-  const animationFrameId = useRef<number | undefined>(undefined);
+  const animationFrameId = useRef<number | null>(null);
 
   const handleScroll = useCallback(() => {
     if (mainReference.current) {
       const scrollY = window.scrollY;
       mainReference.current.style.maskPosition = `0 ${scrollY}px`;
     }
-    animationFrameId.current = undefined;
+    animationFrameId.current = null;
   }, [mainReference]);
 
   const onScroll = useCallback(() => {
-    if (animationFrameId.current === undefined) {
+    if (animationFrameId.current === null) {
       animationFrameId.current = requestAnimationFrame(handleScroll);
     }
   }, [handleScroll]);
@@ -23,8 +23,7 @@ const useScrollMaskEffect = (mainReference: RefObject<HTMLElement>) => {
 
     return () => {
       window.removeEventListener('scroll', onScroll);
-
-      if (animationFrameId.current !== undefined) {
+      if (animationFrameId.current !== null) {
         cancelAnimationFrame(animationFrameId.current);
       }
     };
