8e0c5c892d8211290b4cb55d05bdb1f8be10ee7d
Petr Grabovský <petr@grabovsky.tech>
2024-11-28 17:25:42 +0100

    feat: Vytvoř a integruj základ komponenty Header

--- layout/components/ClientLayout.tsx
+++ layout/components/ClientLayout.tsx
@@ -1,10 +1,16 @@
 import { PropsWithChildren } from 'react';
 
+import Logo from '@/shared/components/Logo';
+
+import Header from './Header';
+
 const ClientLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <header className='h-16 w-full bg-neutral-900 bg-opacity-50'>Header</header>
-      <main className='w-full flex-grow'>{children}</main>
+      <Header>
+        <Logo />
+      </Header>
+      <main className='mb-12 mt-28 w-full flex-grow'>{children}</main>
       <footer className='h-10 w-full bg-neutral-900 bg-opacity-50'>Footer</footer>
     </>
   );
---
+++ layout/components/Header.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+import { PropsWithChildren } from 'react';
+
+const Header = ({ children }: PropsWithChildren) => {
+  return (
+    <header
+      className={clsx(
+        'fixed inset-0 h-16 w-full border-b border-neutral-900 bg-neutral-950 bg-opacity-30',
+        'py-2'
+      )}
+    >
+      <div className='container h-full w-full'>{children}</div>
+    </header>
+  );
+};
+
+export default Header;
