e335e15cd15dcbd096853ca506156c21439e546a
Petr Grabovský <petr@grabovsky.tech>
2024-11-28 13:08:45 +0100

    feat: Vytvoř základní obsah pro stránku Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,44 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section>
+        <h2 className='text-xl text-gray-400'>Section 1</h2>
+        <p className='text-gray-300'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed autem ratione, molestias quam
+          error iste cupiditate exercitationem ea dolore rem laudantium ullam nemo! Rerum nemo
+          beatae autem nisi eos accusantium. Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Suscipit unde ullam atque? Aperiam eaque quis autem illum quam quibusdam, distinctio
+          aspernatur dolorem consequuntur ipsam dolor quia reprehenderit minima odit hic. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Maiores quidem dolor, animi magni
+          illum, impedit quae fugiat non nemo repellat, est recusandae odio autem! Aut qui repellat
+          itaque. Ratione, animi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          in distinctio officia molestias cupiditate enim non debitis, commodi repellendus totam
+          sunt ipsa nihil nemo nobis quasi eum, voluptatibus cum est! Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. A, voluptate similique! Ipsa ab voluptatem nemo quaerat sit
+          eaque consectetur totam quia nobis, rem enim doloremque minus, nisi sed distinctio
+          necessitatibus?
+        </p>
+      </section>
+      <section>
+        <h2 className='text-xl text-gray-400'>Section 2</h2>
+        <p className='text-gray-300'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed autem ratione, molestias quam
+          error iste cupiditate exercitationem ea dolore rem laudantium ullam nemo! Rerum nemo
+          beatae autem nisi eos accusantium. Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Suscipit unde ullam atque? Aperiam eaque quis autem illum quam quibusdam, distinctio
+          aspernatur dolorem consequuntur ipsam dolor quia reprehenderit minima odit hic. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Maiores quidem dolor, animi magni
+          illum, impedit quae fugiat non nemo repellat, est recusandae odio autem! Aut qui repellat
+          itaque. Ratione, animi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          in distinctio officia molestias cupiditate enim non debitis, commodi repellendus totam
+          sunt ipsa nihil nemo nobis quasi eum, voluptatibus cum est! Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. A, voluptate similique! Ipsa ab voluptatem nemo quaerat sit
+          eaque consectetur totam quia nobis, rem enim doloremque minus, nisi sed distinctio
+          necessitatibus?
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default Home;
